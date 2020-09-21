b4dfc0a743aaaaff5076a78ddba42b9cb8e63120
Malomalsky <apgreydd@gmail.com>
2020-09-21 13:49:08 +0300

    fixing

---
+++ url_shortener/createdb.py
@@ -0,0 +1,9 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+from url_shortener import create_app
+from url_shortener.extensions import db, ma
+from url_shortener.models import Link
+
+db.create_all(app=create_app())
